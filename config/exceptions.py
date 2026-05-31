d0134b170896093e4f441e4d8ec5dc435452bb95
Rishabh Sethi <rishabhsethi02@gmail.com>
2026-05-31 08:02:21 +0000

    Added exception handling

---
+++ config/exceptions.py
@@ -0,0 +1,27 @@
+class TradingPlatformException(Exception):
+
+    def __init__(self,message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"TradingPlatformException: {self.message}."
+    
+class NetworkTimeoutException(TradingPlatformException):
+    def __init__(self,message="Network timed out"):
+        super().__init__(message)
+    
+
+
+def handle_system_error(func):
+    def wrapper(*args,**kwargs):
+        try:
+            return func(*args,**kwargs)
+        except NetworkTimeoutException:
+            print("[ERROR] Network timeout detected. Initiating circuit breaker protocols...")
+    return wrapper
+
+@handle_system_error
+def risky_op():
+    raise NetworkTimeoutException()
+risky_op()
